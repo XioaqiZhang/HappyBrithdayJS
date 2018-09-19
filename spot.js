8867d2e794a57a263e60e22ad93d29b24cf1e15c
ZhangXiaoqi <603228547@qq.com>
2018-09-19 15:45:28 +0800

    Add files via upload
    first upload

---
+++ spot.js
@@ -0,0 +1,26 @@
+﻿(function() {
+    'use strict';
+
+    var canvas = document.getElementById('canvas');
+
+    var engine = new Shape.Engine(canvas);
+    var promise = new Promise((resolve) => { resolve(); });
+    
+    //document.getElementById('go').addEventListener('click', start);
+    
+    function start() {
+       // document.getElementById('go').removeEventListener('click', start);
+        promise.then(() => engine.toText('亲爱的'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('生日快乐！'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('LOVE'))
+        .then(() => engine.shake())
+        .then(() => engine.toText('YOU !'))
+        .then(() => engine.shake())
+        .then(() => engine.clear())
+        .then(() => document.getElementById('go').addEventListener('click', start));
+    }
+
+	start();
+})();
