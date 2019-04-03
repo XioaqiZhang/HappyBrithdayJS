af8fa8a24937074e2d674e32b654d6e4cd7fdcd8
ZhangXiaoqi <603228547@qq.com>
2019-04-03 21:49:43 +0800

    Update spot.js

--- spot.js
+++ spot.js
@@ -10,14 +10,14 @@
     
     function start() {
        // document.getElementById('go').removeEventListener('click', start);
-        promise.then(() => engine.toText('亲爱的'))
-		.then(() => engine.shake())
-        .then(() => engine.toText('小徒弟'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('生日快乐！'))
-        .then(() => engine.shake())
-        .then(() => engine.toText('天天开心！'))
-        .then(() => engine.shake())
+//         promise.then(() => engine.toText('亲爱的'))
+// 		.then(() => engine.shake())
+//         .then(() => engine.toText(''))
+//         .then(() => engine.shake())
+//         .then(() => engine.toText('生日快乐！'))
+//         .then(() => engine.shake())
+//         .then(() => engine.toText('天天开心！'))
+//         .then(() => engine.shake())
         .then(() => engine.clear())
         .then(() => document.getElementById('go').addEventListener('click', start));
     }
